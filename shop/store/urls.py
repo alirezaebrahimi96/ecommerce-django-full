8fa99b92fe089613e496831fbacbed5e6d730308
alirezaebrahimi96 <ar.ebrahimi96@gmail.com>
2021-08-09 02:29:19 +0430

    add login

--- shop/store/urls.py
+++ shop/store/urls.py
@@ -8,5 +8,5 @@ app_name = 'store'
 urlpatterns = [
     #path('', views.homepage, name='homepage'),
     path("register/", views.register_request, name="register"),
-
+    path("login", views.login_request, name="login")
 ]
